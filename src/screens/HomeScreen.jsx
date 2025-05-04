7ae706b500248508ee6f88d65a5649eb55c84f71
Osama Javed <osamajaved2002@gmail.com>
2025-05-05 00:15:37 +0500

    commit

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,4 +1,5 @@
 import {
+  Image,
   StyleSheet,
   Text,
   TextInput,
@@ -6,6 +7,7 @@ import {
   View,
 } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import LinearGradient from 'react-native-linear-gradient';
 
 const HomeScreen = () => {
   return (
@@ -26,6 +28,8 @@ const HomeScreen = () => {
         <Text style={styles.locationText}>Kansas City, USA</Text>
       </View>
 
+      {/* Search Container */}
+
       <View style={styles.searchContainer}>
         <TextInput
           style={styles.searchInput}
@@ -34,6 +38,40 @@ const HomeScreen = () => {
         />
         <View style={styles.searchIconWrapper}>
           <Ionicons name="search" size={20} color="#000" />
+        </View>
+      </View>
+
+      {/* Card */}
+      <LinearGradient colors={['#C2185B', '#25050F']} style={styles.card}>
+        <View style={styles.cardText}>
+          <Text style={styles.cardHeading}>Give Bully A Loving Home</Text>
+          <Text style={styles.cardDescription}>
+            They will return the love unconditionally. Adopt today and make the
+            difference.
+          </Text>
+        </View>
+
+        <Image
+          source={require('../../assets/images/dogpic.png')}
+          style={styles.cardImage}
+        />
+      </LinearGradient>
+
+      {/* Explore Section */}
+      <Text style={styles.exploreTitle}>Explore Popular Categories</Text>
+
+      <View style={styles.categoryGrid}>
+        <View style={styles.categoryBox}>
+          <Text style={styles.categoryText}>Dogs</Text>
+        </View>
+        <View style={styles.categoryBox}>
+          <Text style={styles.categoryText}>Supplies</Text>
+        </View>
+        <View style={styles.categoryBox}>
+          <Text style={styles.categoryText}>Adoption</Text>
+        </View>
+        <View style={styles.categoryBox}>
+          <Text style={styles.categoryText}>Veterinary</Text>
         </View>
       </View>
     </View>
@@ -46,6 +84,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: '12%',
+    // backgroundColor:"#FFFFFF"
   },
 
   appBar: {
@@ -95,5 +134,99 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  card: {
+    height: '18%',
+    flexDirection: 'row',
+    // position: 'relative',
+    // justifyContent: 'space-between',
+    paddingLeft: 15,
+    justifyContent: 'flex-start',
+    // overflow: 'hidden',
+    // alignItems: 'flex-start',
+    borderRadius: 10,
+    marginHorizontal: '5%',
+    marginTop: 30,
+    // padding: 15,
+    paddingHorizontal: 15,
+    // paddingVertical: 15,
+    elevation: 4,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+  },
+
+  cardText: {
+    // flex: 1,
+    // paddingRight: 0,
+    zIndex: 2,
+    width: '80%',
+    paddingTop: 20,
+  },
+
+  cardHeading: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+    paddingBottom: 18,
+  },
+
+  cardDescription: {
+    fontSize: 14,
+    color: '#E0E4E8',
+    lineHeight: 20,
+  },
+
+  cardImage: {
+    position: 'absolute',
+
+    right: 0,
+    top: -8,
+    width: '58%',
+    height: '135%',
+    zIndex: 1,
+    opacity: 0.95,
+    // borderRadius: 10,
+    resizeMode: 'contain',
+    opacity: 0.95,
+  },
+  exploreTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#000',
+    marginHorizontal: '5%',
+    marginTop: 25,
+    marginBottom: 10,
+    textTransform: 'uppercase',
+  },
+
+  categoryGrid: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    marginHorizontal: '5%',
+  },
+
+  categoryBox: {
+    width: '47%', // 2 boxes per row with spacing
+    aspectRatio: 2.2,
+    backgroundColor: '#0B1E3E',
+    borderRadius: 5,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    paddingBottom: 18,
+    marginBottom: 15,
+    elevation: 2,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.1,
+    shadowRadius: 3,
+  },
+
+  categoryText: {
+    fontSize: 16,
+    fontWeight: '600',
+    color: '#fff',
   },
 });
